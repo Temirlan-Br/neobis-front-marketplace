ab255f64cd296a50f680be392cb309968e8bd370
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-22 18:11:04 +0600

    Update login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
 import LogoBlock from '../../components/LogoBlock/LogoBlock';
+
+import publicPassword from '../../assets/eye.png';
+import hiddenPassword from '../../assets/blind.png';
 import './Login.css';
 
 const Login = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const switchVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
   return (
     <div className="container">
       <LogoBlock />
+      <div className="form__block">
+        <div className="form">
+          <input
+            type="text"
+            placeholder="Имя пользователя"
+            className="form__input"
+          />
+          <div className="password__block">
+            <input
+              type={passwordVisible ? 'text' : 'password'}
+              placeholder="Пароль"
+              className="form__input"
+            />
+            <img
+              onClick={switchVisibility}
+              src={passwordVisible ? publicPassword : hiddenPassword}
+              alt="eye"
+            />
+          </div>
+          <button className="btn-to-log">Войти</button>
+        </div>
+        <Link to={'/registr'} className="link-to">
+          Зарегистрироваться
+        </Link>
+      </div>
     </div>
   );
 };
