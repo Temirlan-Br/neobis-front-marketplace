c5820a6e09f3f4168254a17358927a460fc47a2e
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-25 23:47:33 +0600

    Request Login API

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,35 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoBlock from '../../components/LogoBlock/LogoBlock';
-
 import publicPassword from '../../assets/eye.png';
 import hiddenPassword from '../../assets/blind.png';
 import './Login.css';
+import { login } from '../../api/api';
 
 const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const payload = {
+      username: userName,
+      password: password,
+    };
+    login(payload)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log('Good Job');
+          //   navigate('/welc');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   const switchVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
@@ -18,12 +39,16 @@ const Login = () => {
       <div className="form__block">
         <div className="form">
           <input
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
             type="text"
             placeholder="Имя пользователя"
             className="form__input"
           />
           <div className="password__block">
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               type={passwordVisible ? 'text' : 'password'}
               placeholder="Пароль"
               className="form__input"
@@ -34,7 +59,9 @@ const Login = () => {
               alt="eye"
             />
           </div>
-          <button className="btn-to-log">Войти</button>
+          <button onClick={onSubmit} className="btn-to-log">
+            Войти
+          </button>
         </div>
         <Link to={'/registr'} className="link-to">
           Зарегистрироваться
