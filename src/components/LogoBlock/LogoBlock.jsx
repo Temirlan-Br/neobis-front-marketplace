646e1c55ef02e89397caf093106fa2fd920463ba
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-25 23:43:24 +0600

    Fix strange error background img

--- src/components/LogoBlock/LogoBlock.jsx
+++ src/components/LogoBlock/LogoBlock.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
+import logoBG from '../../assets/logoBG.png';
 import './LogoBlock.css';
 
 const LogoBlock = () => {
   return (
     <div className="logo__block">
-      <img src={logo} alt="Logo Mobi Market" />
+      <div class="background-image">
+        <img src={logoBG} alt="BG" />
+      </div>
+      <img src={logo} alt="Logo Mobi Market" className="logo" />
     </div>
   );
 };
