9a7471fdc50146f2ba2d58f97927a3a6313408eb
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-25 23:49:05 +0600

    Add route Main pages, base style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
+import Main from './pages/Main/Main';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/registr" element={<SignUp />} />
+          <Route path="/main" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
