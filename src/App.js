e449c6122d83936b9eb81a6810b441ffdd8e5959
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-26 13:58:22 +0600

    Add password page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 import Main from './pages/Main/Main';
+import SignUpPassword from './pages/SignUpPassword/SignUpPassword';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/registr" element={<SignUp />} />
+          <Route path="/registr2" element={<SignUpPassword />} />
           <Route path="/main" element={<Main />} />
         </Routes>
       </BrowserRouter>
