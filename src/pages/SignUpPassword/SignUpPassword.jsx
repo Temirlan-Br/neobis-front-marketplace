7eb0db43c49e32cb4b45c555daea1b55acc8cc18
Temirlan-Br <barakov.temirlan@gmail.com>
2023-12-29 19:21:01 +0600

    Add registration request

--- src/pages/SignUpPassword/SignUpPassword.jsx
+++ src/pages/SignUpPassword/SignUpPassword.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import LogoBlock from '../../components/LogoBlock/LogoBlock';
 import arrow from '../../assets/arrow.png';
-import password from '../../assets/password.png';
+import passwordImg from '../../assets/password.png';
 import './SignUpPassword.css';
+import { register } from '../../api/api';
 
 const SignUpPassword = () => {
+  const [password, setPassword] = useState('');
+  const [checkPassword, setCheckPassword] = useState('');
+  const navigate = useNavigate();
+
+  const passwordData = {
+    password: password,
+    password_confirm: checkPassword,
+  };
+  const storedUserData = JSON.parse(localStorage.getItem('userData'));
+
+  const payload = { ...storedUserData, ...passwordData };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    register(payload)
+      .then((response) => {
+        if (response.status === 201) {
+          navigate('/main');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="container">
       <LogoBlock />
@@ -18,25 +44,30 @@ const SignUpPassword = () => {
         </Link>
         <h2 className="reg__title">Регистрация</h2>
         <div className="form">
-          <img src={password} alt="" />
+          <img src={passwordImg} alt="" />
           <h3 className="title">Придумайте пароль</h3>
           <h4 className="description">
             Минимальная длина — 8 символов. Для надежности пароль должен
             содержать буквы и цифры.
           </h4>
           <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             type="password"
             placeholder="Введите пароль"
             className="password__input"
           />
           <input
+            onChange={(e) => setCheckPassword(e.target.value)}
+            value={checkPassword}
             type="password"
             placeholder="Подтвердите пароль"
             className="password__input"
           />
-          <Link to={'/registr2'} className="link-to">
-            <button className="btn-to-log">Далее</button>
-          </Link>
+
+          <button onClick={onSubmit} className="btn-to-log">
+            Далее
+          </button>
         </div>
       </div>
     </div>
